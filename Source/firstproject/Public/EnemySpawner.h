7e06f4cbf0f066ece9fdcec6f43f97bd14bb0892
ChrisD0904 <christosdemetriades2005@gmail.com>
2025-09-24 01:25:37 +0200

    Refactor comments in EnemySpawner.h for clarity

--- Source/firstproject/Public/EnemySpawner.h
+++ Source/firstproject/Public/EnemySpawner.h
@@ -12,30 +12,23 @@ class FIRSTPROJECT_API AEnemySpawner : public AActor
 	GENERATED_BODY()
 
 private:
-	/** Have we ever spawned (for the first‑time only)? */
 	bool bHasEverSpawned = false;
 
-	/** Are enemies currently active (shown & ticking)? */
 	bool bIsActive = false;
 
 	
 public:	
-	// Sets default values for this actor's properties
 	AEnemySpawner();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	void CheckPlayerDistance();
 
-	/** Only called once ever to populate the pool */
 	void SpawnEnemies();
 
-	/** Show, tick, and collide your pooled enemies */
 	void ActivateEnemies();
 
-	/** Hide, untick, and disable collision */
 	void DeactivateEnemies();
 
 
@@ -50,15 +43,15 @@ public:
 	UPROPERTY()
 	TArray<USceneComponent*> SpawnPoints;
 
-	/** What enemy to spawn */
+	// What enemy to spawn
 	UPROPERTY(EditAnywhere, Category = "Spawner")
 	TSubclassOf<AActor> EnemyClass;
 
-	/** Distance at which to spawn */
+	// Distance at which an enemy will spawn
 	UPROPERTY(EditAnywhere, Category = "Spawner", meta = (ClampMin = "0.0"))
 	float SpawnRadius = 2000.f;
 
-	/** Distance at which to despawn (should be ≥ SpawnRadius) */
+	// Despawn distance
 	UPROPERTY(EditAnywhere, Category = "Spawner", meta = (ClampMin = "0.0"))
 	float DespawnRadius = 3000.f;
 
