258317855b6d9b7b2af82bc0eb5498823d46e3f3
ChrisD0904 <christosdemetriades2005@gmail.com>
2025-09-24 01:22:16 +0200

    Clean up comments in TestPawn.cpp
    Removed unnecessary comments and improved code clarity.

--- Source/firstproject/Private/TestPawn.cpp
+++ Source/firstproject/Private/TestPawn.cpp
@@ -1,12 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TestPawn.h"
 
-// Sets default values
 ATestPawn::ATestPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -18,14 +13,13 @@ void ATestPawn::BeginPlay()
 	
 }
 
-// Called every frame
 void ATestPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
+// Binds functionality to input
 void ATestPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
