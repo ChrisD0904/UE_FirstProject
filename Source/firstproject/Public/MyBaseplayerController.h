f03eedad8ec388b4d048ec566e15b1be067f1b13
ChrisD0904 <christosdemetriades2005@gmail.com>
2025-09-24 01:26:14 +0200

    Removed unnecessary comments from MyBaseplayerController.h
    Removed copyright notice comment from header file.

--- Source/firstproject/Public/MyBaseplayerController.h
+++ Source/firstproject/Public/MyBaseplayerController.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,9 +5,7 @@
 #include "Blueprint/UserWidget.h"
 #include "MyBaseplayerController.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class FIRSTPROJECT_API AMyBaseplayerController : public APlayerController
 {
